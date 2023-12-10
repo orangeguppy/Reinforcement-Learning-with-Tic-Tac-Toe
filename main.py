c7471ac11e5c8bdb3a317d43fb0d93fa30ce619b
orangeguppy <vuwet467@gmail.com>
2023-12-10 21:27:01 +0800

    Adding documentation

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-import players
-from ui import UI
-from game_controller import Game_Controller
-from game_logic import Game_Logic
 import logging
 
-def random_players():
-    playerX = players.RandomPlayer()
-    playerO = players.RandomPlayer()
-    ui = ui.UI()
+from game_controller import Game_Controller
+from game_logic import Game_Logic
+import players
+from ui import UI
 
-    game_logic = Game_Logic(playerX, playerO)
-
-    game_controller = Game_Controller(game_logic, ui)
-    game_controller.play_game()
-
-# Training the QLearners
+'''Method to train the Q-Learning agents'''
 def train(num_epochs):
     playerX = players.QLearner()
     playerO = players.QLearner()
@@ -28,7 +19,7 @@ def train(num_epochs):
     playerX.saveQtable("playerXstates")
     playerO.saveQtable("playerOstates")
 
- # QLearner vs Human
+'''Method to play against trained AI Q-Learner'''
 def play_against_ai():
     ui = UI()
     playerX = players.QLearner()
@@ -41,6 +32,7 @@ def play_against_ai():
     game_controller.play_game()
     print(game_controller.game_logic.board)
 
+# The code below trains the Q-Learners and then lets you play against them
 train(50000)
 while(True):
     play_against_ai()
